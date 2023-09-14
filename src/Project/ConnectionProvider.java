ec9bf39d9b1270b13a84e289e5e970e83fcb5fb2
SanthoshKannan-S <santhoshkannan01072002@gmail.com>
2023-09-14 21:59:50 +0530

    first push about tourism management system

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,21 @@
+package Project;
+import java.sql.*;
+/**
+ *
+ * @author BaLa
+ */
+public class ConnectionProvider {
+    public static Connection getCon()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/tms","root","santhosh12345@");
+            return con;
+        }
+        catch(Exception e)
+        {
+            return null;
+        }
+    }
+}
